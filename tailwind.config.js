c5f1725769dc2c74736ba1c1898cca6af99c9e48
arian <arian.maki@yahoo.com>
2025-07-28 06:37:46 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        myfont: ["Dongle", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
