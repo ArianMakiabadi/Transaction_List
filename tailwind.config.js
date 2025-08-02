07405cb8fa7491149d196fee3201e6f4c2487a31
arian <arian.maki@yahoo.com>
2025-08-02 16:20:12 +0200

    made tailwind track js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.html"],
-  safelist: [
-    // Table Properties
-    "h-10",
-    "text-center",
-
-    // Red styles
-    "ring-red-300",
-    "bg-red-200",
-    "text-red-500",
-    "text-red-600",
-
-    // Green styles
-    "ring-green-300",
-    "bg-green-200",
-    "text-green-500",
-    "text-green-600",
-
-    // purple styles
-    "bg-violet-500 ",
-    "px-3",
-    "py-0.5",
-    "text-white",
-
-    // Transitions
-    "transform",
-    "transition-transform",
-    "duration-300",
-    "rotate-180",
-  ],
+  content: ["./public/**/*.html", "./src/js/*.js"],
   theme: {
     extend: {
       fontFamily: {
